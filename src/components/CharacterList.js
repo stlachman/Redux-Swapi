a547c5eada1e55e4165eecca3a28b16abdbd051c
Seth Lachman <lachmanseth17@gmail.com>
2019-05-15 18:39:47 -0400

    Updated code to use characterlistview.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,39 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Loader from 'react-loader-spinner';
 
-import { getCharacters } from '../actions';
-import './CharacterList.css'
+import './CharacterList.css';
 import Character from './Character';
 
-class CharacterList extends React.Component {
-	componentDidMount() {
-		this.props.getCharacters();
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<h1>Star Wars Characters</h1>
-				{this.props.isLoading && <Loader type="Puff" color="#42f1f4" height={80} width={80} />}
-				<ul className="list">
-					{this.props.characters &&
-						this.props.characters.map((character) => {
-							return <Character key={character.name} character={character} />;
-						})}
-					{this.props.error && <p>{this.props.error}</p>}
-				</ul>
-			</div>
-		);
-	}
-}
-
-const mapStateToProps = (state) => {
-	return {
-		characters: state.charsReducer.characters,
-		isLoading: state.charsReducer.isLoading,
-		error: state.charsReducer.error
-	};
+const CharacterList = (props) => {
+	return (
+		<div className="container">
+			<h1>Star Wars Characters</h1>
+			<ul className="list">
+				{props.characters &&
+					props.characters.map((character) => {
+						return <Character key={character.name} character={character} />;
+					})}
+          {props.error && <p>{props.error}</p>}
+			</ul>
+		</div>
+	);
 };
 
-export default connect(mapStateToProps, { getCharacters })(CharacterList);
+export default CharacterList;
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,35 +1,40 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
 
-import { CharacterList } from "../components";
+import { CharacterList } from '../components';
 // import actions
+import { getCharacters } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  componentDidMount() {
-    // call our action
-  }
+	componentDidMount() {
+		// call our action
+		this.props.getCharacters();
+	}
 
-  render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
-    }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
-  }
+	render() {
+		if (this.props.isLoading) {
+			return <Loader type="Puff" color="#42f1f4" height={80} width={80} />;
+		}
+
+		return (
+			<div className="CharactersList_wrapper">
+				<CharacterList error={this.props.error} characters={this.props.characters} />
+			</div>
+		);
+	}
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
-export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+const mapStateToProps = (state) => {
+	return {
+		characters: state.charsReducer.characters,
+		isLoading: state.charsReducer.isLoading,
+		error: state.charsReducer.error
+	};
+};
+
+export default connect(mapStateToProps, { getCharacters })(CharacterListView);

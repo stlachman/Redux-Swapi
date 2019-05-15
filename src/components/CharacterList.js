10739413018ce405e59a4f0c3dfb41051cb6e843
Seth Lachman <lachmanseth17@gmail.com>
2019-05-15 16:27:19 -0400

    Added basic styles to project.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react';
 import { connect } from 'react-redux';
-import Loader from "react-loader-spinner";
+import Loader from 'react-loader-spinner';
 
 import { getCharacters } from '../actions';
-import Character from "./Character";
+import './CharacterList.css'
+import Character from './Character';
 
 class CharacterList extends React.Component {
-  componentDidMount() {
-    this.props.getCharacters();
-  }
+	componentDidMount() {
+		this.props.getCharacters();
+	}
 
-  render() {
-    return (
-      <div>
-        {this.props.isLoading && (
-          <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
-        )}
-        <ul>
-          {this.props.characters && this.props.characters.map(character => {
-            return <Character key={character.name} character={character} />;
-          })}
-          {this.props.error && <p>{this.props.error}</p>}
-        </ul>
-      </div>
-    );
-  }
-  
-};
+	render() {
+		return (
+			<div className="container">
+				<h1>Star Wars Characters</h1>
+				{this.props.isLoading && <Loader type="Puff" color="#42f1f4" height={80} width={80} />}
+				<ul className="list">
+					{this.props.characters &&
+						this.props.characters.map((character) => {
+							return <Character key={character.name} character={character} />;
+						})}
+					{this.props.error && <p>{this.props.error}</p>}
+				</ul>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = (state) => {
-  return {
-    characters: state.charsReducer.characters,
-    isLoading: state.charsReducer.isLoading,
-    error: state.charsReducer.error
-  }
-}
+	return {
+		characters: state.charsReducer.characters,
+		isLoading: state.charsReducer.isLoading,
+		error: state.charsReducer.error
+	};
+};
 
 export default connect(mapStateToProps, { getCharacters })(CharacterList);
